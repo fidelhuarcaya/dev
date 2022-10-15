14bb32f58711e250cd27a51d772f1d7ca06bc044
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2022-10-15 11:45:14 -0500

    changed icon

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  isVersionDesktop = false;
+  constructor(
+    public device: DeviceDetectorService, ) { 
+      this.isVersionDesktop=device.isDesktop();
+    }
 
   ngOnInit(): void {
+  }
+  getPadding(){
+    if(this.isVersionDesktop)
+    return '12%';
+    else return '0%';
   }
 
 }
