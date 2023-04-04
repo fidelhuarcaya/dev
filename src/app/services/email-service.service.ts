d0ef6d8108a1f706178f18dd1787ae7eb977525d
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2023-04-04 16:47:23 -0500

    feat: change base url

--- src/app/services/email-service.service.ts
+++ src/app/services/email-service.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { retry, catchError, map } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
 import { Email } from '../model/Message';
 
@@ -10,38 +9,11 @@ import { Email } from '../model/Message';
 export class EmailService {
   base_url="https://ms-emails.azurewebsites.net/emails/send"
  
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-     
 
-    }),
-  };
   constructor(private http: HttpClient) { }
 
   sendEmail(email:Email): Observable<Email>{
     return this.http
-      .post<Email>(this.base_url,
-         JSON.stringify(email),this.httpOptions);
-  }
-  greeting():Observable<String>{
-    return this.http.get<String>(this.base_url).pipe(retry(1), catchError(this.errorHandl));
-  }
-
-
-
-  errorHandl(error: { error: { message: string; }; status: any; message: any; }) {
-    let errorMessage = '';
-    if (error.error instanceof ErrorEvent) {
-      // Get client-side error
-      errorMessage = error.error.message;
-    } else {
-      // Get server-side error
-      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-    }
-    console.log(errorMessage);
-    return throwError(() => {
-      return errorMessage;
-    });
+      .post<Email>(this.base_url,email);
   }
 }
