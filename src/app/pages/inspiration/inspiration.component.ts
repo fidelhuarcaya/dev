08024d405f7de80ebfd60d5898198e65333d8e55
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2023-04-19 10:51:06 -0500

    feat: add mathview library for android.

--- src/app/pages/inspiration/inspiration.component.ts
+++ src/app/pages/inspiration/inspiration.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-inspiration',
   templateUrl: './inspiration.component.html',
   styleUrls: ['./inspiration.component.scss']
 })
-export class InspirationComponent {
+export class InspirationComponent implements OnInit{
+  isMobile = false;
+  
+  constructor( private deviceService: DeviceDetectorService,) { }
 
+  ngOnInit(): void {
+    this.isMobile = this.deviceService.isMobile();
+  }
 }
